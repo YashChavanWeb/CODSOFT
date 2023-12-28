3046a9f6be4da5999789ac55769ccd8a86747239
Yash Chavan <112185595+YashChavanWeb@users.noreply.github.com>
2023-12-28 12:50:56 +0530

    Update script.js

--- 02_Portfolio Website/script.js
+++ 02_Portfolio Website/script.js
@@ -59,6 +59,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+// ----------------------------- Resume Download Function ----------------------------- 
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element
+    var downloadButton = document.getElementById('downloadButton');
+
+    // Add a click event listener to the button
+    downloadButton.addEventListener('click', function() {
+        // Replace the URL below with the actual link to your portfolio file
+        var portfolioUrl = 'Images/Yash Chavan - Portfolio.pdf';
+
+        // Create a temporary link element
+        var link = document.createElement('a');
+
+        // Set the href attribute of the link to the portfolio URL
+        link.href = portfolioUrl;
+
+        // Set the download attribute to prompt the user to download the file
+        link.download = 'YourPortfolio.pdf';
+
+        // Append the link to the document
+        document.body.appendChild(link);
+
+        // Trigger a click event on the link to initiate the download
+        link.click();
+
+        // Remove the link from the document
+        document.body.removeChild(link);
+    });
+});
+
+
+
+
+
+
+
 
 // ----------------------------- About Buttons Functionality ----------------------------- 
 
